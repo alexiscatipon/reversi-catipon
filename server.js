193c096979fcdfb3c1b8751ccae3265b7c38dd86
Alexis Catipon <acatipon@uci.edu>
2023-05-26 22:50:02 -0700

    no message

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 let static = require('node-static');
+
 let http = require('http');
+
 let port = process.env.PORT;
 let directory = __dirname + '/public';
+
 if ((typeof port == 'undefined') || (port === null)) {
     port = 8080;
     directory = './public';
@@ -22,7 +25,7 @@ let app = http.createServer(
 console.log('The server is running');
 
 
-
+let players = [];
 
 const { Server } = require("socket.io");
 const io = new Server(app);
@@ -39,9 +42,7 @@ io.on('connection', (socket) => {
 
     serverLog('a page connected to the server: ' + socket.id);
 
-    socket.on('disconnect', () => {
-        serverLog('a page disconnected from the server:' + socket.id);
-    });
+
 
     socket.on('join_room', (payload) => {
         serverLog('Server received a command', '\'join_room\'', JSON.stringify(payload));
@@ -84,17 +85,45 @@ io.on('connection', (socket) => {
                 serverLog('join_room command failed', JSON.stringify(response));
             }
             else {
-                response = {};
-                response.result = 'success';
-                response.room = room;
-                response.username = username;
-                response.count = sockets.length;
+                players[socket.id] = {
+                    username: username,
+                    room: room
+                }
+                for (const member of sockets) {
+                    response = {
+                        result: 'succes',
+                        socket_id: member.id,
+                        room: players[member.id].room,
+                        username: players[member.id].username,
+                        count: sockets.length
+                    }
 
-                io.of('/').to(room).emit('join_room_response', response);
-                serverLog('join_room succeeded ', JSON.stringify(response));
+                    io.of('/').to(room).emit('join_room_response', response);
+                    serverLog('join_room succeeded ', JSON.stringify(response));
+                }
+
             }
         });
     });
+
+    socket.on('disconnect', () => {
+        serverLog('a page disconnected from the server:' + socket.id);
+        if((typeof players [socket.id] != 'undefined') && (players[socket.id] != null)){
+            let payload = {
+                username: players[socket.id].username,
+                room: players[socket.id].room,
+                count: Object.keys(players).length - 1,
+                socket_id: socket.id
+            };
+            let room = players[socket.id].room;
+            delete players[socket.id];
+
+
+            io.of("/").to(room).emit('player_disconnected',payload);
+            serverLog('player_disconnected succeeded ',JSON.stringify(payload));
+        }
+    });
+
 
     socket.on('send_chat_message', (payload) => {
         serverLog('Server received a command', '\'send_chat_message\'', JSON.stringify(payload));
